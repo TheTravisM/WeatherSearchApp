d5fec20bd20cd4c0b86a3212426c7d8a2fbb0762
Travis E  Mikolay <TravisEMikolay+GitKraken@Gmail.com>
2021-08-09 14:55:33 -0400

    Text Update

--- app/src/main/java/com/tm/navapp/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/tm/navapp/ui/notifications/NotificationsFragment.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.tm.navapp.R
 import com.tm.navapp.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
